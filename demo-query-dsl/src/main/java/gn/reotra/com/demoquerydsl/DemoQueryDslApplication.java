8899b28a424402ccd0bac125e90a42b1fd5d355e
sory.traore <sorytraore@eti.net.gn>
2021-09-28 12:40:09 +0000

    Initialisation de la démo Query DSL

---
+++ demo-query-dsl/src/main/java/gn/reotra/com/demoquerydsl/DemoQueryDslApplication.java
@@ -0,0 +1,13 @@
+package gn.reotra.com.demoquerydsl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoQueryDslApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoQueryDslApplication.class, args);
+    }
+
+}
---
+++ demo-query-dsl/src/test/java/gn/reotra/com/demoquerydsl/DemoQueryDslApplicationTests.java
@@ -0,0 +1,13 @@
+package gn.reotra.com.demoquerydsl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoQueryDslApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
